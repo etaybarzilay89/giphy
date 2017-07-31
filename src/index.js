de371a64242da5f95ce207c5f767d5f876b9498c
Etay Barzilay <etaybarzilay89@gmail.com>
2017-07-31 09:09:25 +0300

    added webpack config file and added build script to package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 //module giphy
 require('isomorphic-fetch');
 
-// const apiKey2 = 'b6ea48d0a38349fc991b2818f94b8caf';
-
 module.exports = {
   getGiphy,
 };
@@ -12,5 +10,4 @@ function getGiphy(query, apiKey) {
       .then(response => response.json())
       .then(response => response.data[0].images.original.url)
       .catch(error => null);
-  // return `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${giphyKey}&limit=1&offset=0&rating=R&lang=en`;
 }
